e7f7a6c0675e4178a9e37d9f30a5230c9d2072e6
ksvarani204 <146092075+ksvarani@users.noreply.github.com>
2024-12-04 20:32:16 +0530

    Server Files

--- backend/models/user.js
+++ backend/models/user.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
     password: {type: String, required: true},
     address: {type: String, required: true},
     avatar: {type: String, default:"https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png"},
-    role: {type: String, default: "user", enum: ["user","admin"]},
+    role: {type: String, default: "USER", enum: ["USER","ADMIN"]},
     favourites: [{type: mongoose.Types.ObjectId, ref: "books"}],
     cart: [{type: mongoose.Types.ObjectId, ref: "books"}],
     orders: [{type: mongoose.Types.ObjectId, ref: "order"}]
